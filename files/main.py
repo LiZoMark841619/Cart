1ca9085e1b61dc402a2eed0942dc4cb76adad157
LiZoMark841619 <githubs001@gmail.com>
2024-05-25 22:44:34 +0200

    ok

---
+++ files/main.py
@@ -0,0 +1,21 @@
+import Valid, Menu, Item
+
+def main():
+    valid, menu, purchase = Valid(), Menu(), Item()
+    while True:
+        choice = valid.get_valid_number(f'Choose your option from the menu below!\n{menu.options} ', 0, 5)
+        if choice == 0: purchase.budget = valid.get_valid_number('Enter your budget from 10 to 1000 USD! ', 10, 1000)
+        elif choice == 1:
+            options = ['apple', 'orange', 'lemon', 'pineapple', 'banana', 'cherry', 'blackberry']
+            name = valid.get_valid_string(f'Enter the name of the items from {options} options! ', *options)
+            price = valid.get_valid_number('Enter the price of the item from 1 to 100 USD! ', 1, 100)
+            max_amount = int(purchase.budget / price)
+            amount = valid.get_valid_number(f'Enter the amount of the item from 0 to {max_amount}! ', 0, max(0, max_amount))            
+            purchase.item = [name, price, max_amount, amount]
+            purchase.add_item()
+            print('Item added successfully.')
+        elif choice == 2: print(purchase.cart)
+        elif choice == 3: purchase.remove_item()
+        elif choice == 4: print(f'Your current budget available is {purchase.budget} USD.')
+        elif choice == 5: print('Good bye! '); return
+if __name__ == '__main__': main()
---
+++ files/refactored.py
@@ -0,0 +1,65 @@
+class Valid:
+    def get_valid_number(self, prompt: str, min_val: int, max_val: int) -> int:
+        while True:
+            try:
+                value = int(input(prompt))
+                if min_val <= value <= max_val: return value
+                print('Invalid number! ')
+            except ValueError: print('Invalid value! ')
+            
+    def get_valid_string(self, prompt: str, *args) -> str:
+        while True:
+            value = input(prompt).lower()
+            if value in args: return value
+            print('Invalid value! ')
+
+class Menu:
+    def __init__(self) -> None:
+        self._options = {0:'Set budget', 1:'Add item', 2:'View cart', 3:'Remove item', 4:'View budget', 5:'Exit'}
+    
+    def options(self) -> int:
+        return self._options
+    options = property(options)
+    
+    @options.setter
+    def options(self, new_val: dict) -> None:
+        self._options = new_val
+        
+class Budget:    
+    def __init__(self) -> None:
+        self._budget = 0
+    
+    def budget(self) -> int:
+        return self._budget
+    budget = property(budget)
+    
+    @budget.setter 
+    def budget(self, new_val: int) -> None:
+        self._budget = new_val
+
+class Item(Budget):
+    cart: list[dict] = []
+    
+    def item(self) -> dict:
+        return {'name':self._item_name, 'price':self._item_price, 'max_amount':self._max_amount, 'purchasing_amount':self._amount}
+    item = property(item)
+    
+    @item.setter 
+    def item(self, new_vals: list):
+        self._item_name, self._item_price, self._max_amount, self._amount = new_vals
+    
+    def add_item(self) -> None:
+        item = self.item
+        Item.cart.append(item)
+        self._budget = self.budget - item['price'] * item['purchasing_amount']
+    
+    def remove_item(self) -> None:
+        items = Item.cart
+        if not items: print('There is no item in the cart yet! Choose - Add item - from the menu by typing 1! ')
+        else: 
+            delete_item = input('Enter the item you would like to remove! ').lower()
+            for item in items:
+                if delete_item == item['name']:
+                    items.remove(item)
+                    self._budget = self.budget + (item['price'] * item['purchasing_amount'])
+                    print('Item deleted successfully! ')
