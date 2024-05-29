f2c034c8836a98a3bcbe55e691147de18413b67f
LiZoMark841619 <githubs001@gmail.com>
2024-05-29 10:28:13 +0200

    ok

--- files/budget_cart.py
+++ files/budget_cart.py
@@ -4,7 +4,6 @@ class Budget:
     @property
     def budget(self) -> int:
         return self._budget
-    
     @budget.setter 
     def budget(self, new_val: int) -> None:
         self._budget = new_val
@@ -12,16 +11,14 @@ class Budget:
 class Cart(Budget):
     to_purchase: list[dict] = []
     
-    def add_item(self, item_name: str, item_price: int, amount_to_buy: int) -> None:
-        item = {'item_name':item_name, 'item_price':item_price, 'amount_to_buy':amount_to_buy}
+    def add_item(self, item_name: str, item_price: int, quantity_to_buy: int) -> None:
+        item = {'item_name':item_name, 'item_price':item_price, 'quantity_to_buy':quantity_to_buy}
         Cart.to_purchase.append(item)
-        self.budget -= item_price * amount_to_buy
+        self.budget -= item_price * quantity_to_buy
             
-    def remove_item(self, item_name: str) -> None:
+    def remove_item(self, item_name: str, quantity_to_remove: int) -> None:
         for item in Cart.to_purchase:
             if item_name.lower() == item['item_name']:
-                Cart.to_purchase.remove(item)
-                self.budget += item['item_price'] * item['amount_to_buy']
+                item['quantity_to_buy'] -= quantity_to_remove
+                self.budget += item['item_price'] * quantity_to_remove
                 print('Item deleted successfully! ')
-            else:
-                print('Item not found in the cart! ')
--- files/main.py
+++ files/main.py
@@ -1,17 +1,16 @@
 from valid_menu import Valid, Menu
 from budget_cart import Cart
 
-OPTIONS = {'apple', 'orange', 'lemon', 'pineapple', 'banana', 'cherry', 'blackberry'}
+STORE = {'apple':4, 'orange':6, 'lemon':4, 'pineapple':4, 'banana':6, 'cherry':8, 'blackberry':10}
 MIN_BUDGET, MAX_BUDGET = 10, 1000
-MIN_ITEM_PRICE, MAX_ITEM_PRICE = 1, 100
 
 def set_budget(valid, cart) -> None:
     cart.budget = valid.get_valid_number('Enter your budget from 10 to 1000 USD! ', 10, 1000)
 
 def add_item(valid, cart) -> None:
     if cart.budget:
-        item_name = valid.get_valid_string(f'Enter the name of the items from {OPTIONS} options! ', *OPTIONS)
-        item_price = valid.get_valid_number(f'Enter the price of the item from {MIN_ITEM_PRICE} to {MAX_ITEM_PRICE} USD! ', MIN_ITEM_PRICE, MAX_ITEM_PRICE)
+        item_name = valid.get_valid_string(f'Enter the name of the items from {STORE} options! ', *STORE)
+        item_price = STORE[item_name]
         max_amount = int(cart.budget / item_price)
         amount_to_buy = valid.get_valid_number(f'Enter the amount of the item from 0 to {max_amount}! ', 0, max(0, max_amount))
         cart.add_item(item_name, item_price, amount_to_buy)
@@ -23,8 +22,14 @@ def view_cart(cart) -> None:
     print(cart.to_purchase)
 
 def remove_item(valid, cart) -> None:
-    cart.remove_item(valid.get_valid_string(f'Enter the name of the items from {OPTIONS} options! ', *OPTIONS))
-
+    if not Cart.to_purchase: print('You cannot remove items because your cart is Empty! ')
+    else:
+        item_names = [item['item_name'] for item in cart.to_purchase]
+        item_name = valid.get_valid_string(f'Enter the name of the items from {item_names} actual cart! ', *item_names)
+        max_quantity = [item['quantity_to_buy'] for item in cart.to_purchase if item['item_name'] == item_name]
+        quantity_to_remove = valid.get_valid_number('Enter the quantity to remove from the cart! ', 0, max_quantity[0])
+        cart.remove_item(item_name, quantity_to_remove)
+    
 def view_budget(cart) -> None: 
     print(f'Your current budget available is {cart.budget} USD. ')
 
@@ -33,19 +38,10 @@ def exit() -> None:
     
 def main() -> None:
     valid, menu, cart = Valid(), Menu(), Cart()
-    actions = {
-        0: lambda: set_budget(valid, cart),
-        1: lambda: add_item(valid, cart),
-        2: lambda: view_cart(cart),
-        3: lambda: remove_item(valid, cart),
-        4: lambda: view_budget(cart),
-        5: lambda: exit()
-    }
+    actions = {0: lambda: set_budget(valid, cart), 1: lambda: add_item(valid, cart), 2: lambda: view_cart(cart),
+               3: lambda: remove_item(valid, cart), 4: lambda: view_budget(cart), 5: lambda: exit()}
     while True:
         choice = valid.get_valid_number(f'Choose your option from the menu below!\n{menu.options} ', 0, 5)
-        if choice == 5: 
-            exit()
-            break
+        if choice == 5: exit(); break
         actions[choice]()
-        
-if __name__ == '__main__': main()
+if __name__ == '__main__':  main()
