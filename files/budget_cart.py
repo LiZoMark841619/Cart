de6efb9de1b5ccc0a856d68cd9914fab7adf71a2
LiZoMark841619 <githubs001@gmail.com>
2024-05-29 11:14:05 +0200

    ok

--- files/budget_cart.py
+++ files/budget_cart.py
@@ -14,8 +14,7 @@ class Cart(Budget):
     to_purchase: list[dict] = []
     
     def add_item(self, item_name: str, item_price: int, quantity_to_buy: int) -> None:
-        item = {'item_name':item_name, 'item_price':item_price, 'quantity_to_buy':quantity_to_buy}
-        Cart.to_purchase.append(item)
+        Cart.to_purchase.append({'item_name':item_name, 'item_price':item_price, 'quantity_to_buy':quantity_to_buy})
         self.budget -= item_price * quantity_to_buy
             
     def remove_item(self, item_name: str, quantity_to_remove: int) -> None:
@@ -23,4 +22,3 @@ class Cart(Budget):
             if item_name.lower() == item['item_name']:
                 item['quantity_to_buy'] -= quantity_to_remove
                 self.budget += item['item_price'] * quantity_to_remove
-                print('Item deleted successfully! ')
